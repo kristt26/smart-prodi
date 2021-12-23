647e74611b6fa5f6bb29253832a9a9a33e2cfada
Ajenkris Kungkung <kristt26@gmail.com>
2021-12-23 14:31:10 +0900

    gh

--- pencarian.php
+++ pencarian.php
@@ -220,13 +220,13 @@ $kode = $data['kd_pts'];
         $dat = mysqli_fetch_array($dt);
         ?>
                                     <a href="#" data-toggle='modal'
-                                        data-target='#universitas'><?php echo $dat['nama_pts'] ?></a>
+                                        data-target='#universitas<?=$kode?>'><?php echo $dat['nama_pts'] ?></a>
 
                                     <!-- Logout Modal-->
-                                    <div class="modal fade" id="universitas" tabindex="-1" role="dialog"
+                                    <div class="modal fade" id="universitas<?=$kode?>" tabindex="-1" role="dialog"
                                         aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog modal-lg" role="document">
-                                            <div class="modal-content">
+                                            <div class="modal-content bg-warning">
                                                 <div class="modal-header">
                                                     <h5 class="modal-title" id="exampleModalLabel">Universitas Detail
                                                     </h5>
@@ -237,80 +237,40 @@ $kode = $data['kd_pts'];
                                                 </div>
                                                 <div class="modal-body">
                                                     <form>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Nama
-                                                                Universitas</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['nama_pts'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Akreditasi</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['akreditasi'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Alamat</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['alamat'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Kode
-                                                                Pos</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['kode_pos'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Telepon</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['no_telepon'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Email</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['email'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Website</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['laman_web'];?>">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group row">
-                                                            <label for="staticEmail"
-                                                                class="col-sm-4 col-form-label  text-left">Fasilitas</label>
-                                                            <div class="col-sm-8">
-                                                                <input type="text" readonly
-                                                                    class="form-control-plaintext" id="staticEmail"
-                                                                    value=": <?=$dat['fasilitas_kampus'];?>">
-                                                            </div>
-                                                        </div>
+                                                        <table class="table table-bordered bg-warning">
+                                                            <tr>
+                                                                <td width="30%">Nama Universitas</td>
+                                                                <td><?= $dat['nama_pts'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Akreditasi</td>
+                                                                <td><?= $dat['akreditasi'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Alamat</td>
+                                                                <td><?= $dat['alamat'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Kode Pos</td>
+                                                                <td><?= $dat['kode_pos'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Telepon</td>
+                                                                <td><?= $dat['no_telepon'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Email</td>
+                                                                <td><?= $dat['email'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Website</td>
+                                                                <td><?= $dat['laman_web'];?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Fasilitas</td>
+                                                                <td><?= $dat['fasilitas_kampus'];?></td>
+                                                            </tr>
+                                                        </table>
                                                     </form>
                                                     <!-- <table class="text-center table table-bordered" id="dataTable"
                                                         width="100%" cellspacing="0">
